0160dc9676af3a1f7affc6887d850da1c0e081ea
denilson <denilsondslima@gmail.com>
2023-04-17 00:29:29 -0300

    feat: add error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, {json} from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { handleApplicationErrors } from "./middlewares/erros-middleware.js"
+import routes from "./routes/index.js"
 
 const app = express()
 dotenv.config()
 app.use(json())
 app.use(cors())
+app.use(routes)
+app.use(handleApplicationErrors)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running in port: ${port}`))
